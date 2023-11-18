22a1edc9adefee889725fb0e7606773fa39a849a
Temberland <Temberland@users.noreply.github.com>
2023-11-18 21:17:52 +1000

    Add AboutGame

---
+++ CivilizationSwipe/Assets/Scripts/GameLogic/ButtonControllers/SettingButton/AboutGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AboutGame : MonoBehaviour
+{
+    public GameObject Canvas;
+    public static bool active = false;
+    public void Aboutgame()
+    {
+        active = true;
+        EscapeScript.active = false;
+        Canvas.SetActive(true);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (active == true)
+            {
+                active = false;
+                Canvas.SetActive(false);
+            }
+        }
+    }
+}
